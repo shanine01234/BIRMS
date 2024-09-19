b257734c1837a2aaa77178723c859c74065234cc
shanine01234 <rafaelpacino43@gmail.com>
2024-09-19 08:16:34 +0800

    Update cart.php

--- cart.php
+++ cart.php
@@ -99,6 +99,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <title>Bantayan Island Restobar</title>
 
     <!-- Custom fonts for this template-->
+     <link rel="shortcut icon" type="x-icon" href="./img/logo.jpg"
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
