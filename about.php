87796dedc8b66d8b9093aa8a9dca73fc0f71ae3b
shanine01234 <rafaelpacino43@gmail.com>
2024-11-09 08:44:56 +0800

    Update about.php

--- about.php
+++ about.php
@@ -15,7 +15,6 @@ require_once('inc/header.php');
     <title>Bantayan Island Restobar</title>
 
     <!-- Custom fonts for this template-->
-     <link rel="shortcut icon" type="x-icon" href="./img/logo.jpg"
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
