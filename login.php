b2f4d0c190d07893252c221946e72089c78a4b92
shanine01234 <rafaelpacino43@gmail.com>
2024-11-09 17:30:14 +0800

    Update login.php

--- login.php
+++ login.php
@@ -250,7 +250,7 @@ if (isset($_POST['login'])) {
 
    
     <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
+     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.min.js"></script>
