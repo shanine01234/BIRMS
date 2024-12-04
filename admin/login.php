1ec2e6eacc9b9c84f98a1f339b48dbacbd71f7ed
shanine01234 <rafaelpacino43@gmail.com>
2024-12-04 11:01:46 +0800

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -15,6 +15,8 @@ require_once('process/loginAdmin.php');
     <meta name="author" content="">
 
     <title>BIRMS | Admin Login</title>
+ <link rel="icon" type="image/png" href="img/d3f06146-7852-4645-afea-783aef210f8a.jpg" alt="" width="30" height="24" style="border-radius: 100px;">
+    <!-- Custom fonts for this template-->
 
     <!-- Custom fonts for this template-->
     <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
