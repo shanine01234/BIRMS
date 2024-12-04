94a498995462762222944e879bf6a9394925de3b
shanine01234 <rafaelpacino43@gmail.com>
2024-12-04 11:05:13 +0800

    Update resto_pending.php

--- admin/resto_pending.php
+++ admin/resto_pending.php
@@ -18,7 +18,7 @@ if (!isset($_SESSION['id'])) {
     <meta name="author" content="">
 
     <title>BIRMS | Admin</title>
-
+<link rel="icon" type="image/png" href="../img/d3f06146-7852-4645-afea-783aef210f8a.jpg" alt="" width="30" height="24" style="border-radius: 100px;">
     <!-- Custom fonts for this template-->
     <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
