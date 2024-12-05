c2ac2d124e6c2bf7a1efad4e6f14108884856a7a
shanine01234 <rafaelpacino43@gmail.com>
2024-12-05 14:11:38 +0800

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -2,13 +2,20 @@
 require_once('../inc/function.php');
 require_once('process/loginAdmin.php');
 
+// Verify reCAPTCHA response
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['loginAdmin'])) {
+    $recaptchaSecret = '6Ldz7JIqAAAAAIp9MiVvQepNEFe9o0GywFAnBH95'; // Replace with your reCAPTCHA v3 secret key
+    $recaptchaResponse = $_POST['g-recaptcha-response'];
+    
+    // Make a POST request to Google's reCAPTCHA verification API
+    $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret={$recaptchaSecret}&response={$recaptchaResponse}");
+    
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -19,24 +26,31 @@ require_once('process/loginAdmin.php');
 
     <!-- Custom fonts for this template-->
     <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="../css/sb-admin-2.min.css" rel="stylesheet">
 
+    <!-- reCAPTCHA script -->
+    <script src="https://www.google.com/recaptcha/api.js?render=6Ldz7JIqAAAAALCcq3dDLQBNAyHnlcVKyFzuxxBg"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            grecaptcha.ready(function () {
+                grecaptcha.execute('6Ldz7JIqAAAAALCcq3dDLQBNAyHnlcVKyFzuxxBg', { action: 'login' }).then(function (token) {
+                    document.getElementById('g-recaptcha-response').value = token;
+                });
+            });
+        });
+    </script>
 </head>
 
 <body class="bg-gradient-primary">
 
     <div class="container">
-
         <!-- Outer Row -->
         <div class="row justify-content-center">
-
             <div class="col-lg-6">
-
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
@@ -45,7 +59,7 @@ require_once('process/loginAdmin.php');
                                 <div class="p-5">
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-4">Welcome Back Admin!</h1>
-                                        <?=$msgAlert?>
+                                        <?=$msgAlert ?? ''?>
                                     </div>
                                     <form class="user" method="POST">
                                         <div class="form-group">
@@ -57,34 +71,26 @@ require_once('process/loginAdmin.php');
                                             <input type="password" name="password" class="form-control form-control-user"
                                                 id="exampleInputPassword" placeholder="Password" required>
                                         </div>
-
+                                        <input type="hidden" id="g-recaptcha-response" name="g-recaptcha-response">
                                         <button type="submit" name="loginAdmin" class="btn btn-primary btn-user btn-block">Login</button>
-                                        
                                     </form>
                                     <hr>
-                                    
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
             </div>
-
         </div>
-
     </div>
 
     <!-- Bootstrap core JavaScript-->
     <script src="../vendor/jquery/jquery.min.js"></script>
     <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
     <!-- Core plugin JavaScript-->
     <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
-
     <!-- Custom scripts for all pages-->
     <script src="../js/sb-admin-2.min.js"></script>
-
 </body>
 
 </html>
