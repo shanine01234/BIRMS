09a08d2a63353b2a14a73e2e24c3dc1bbbeeb736
shanine01234 <rafaelpacino43@gmail.com>
2024-12-05 14:49:19 +0800

    Update login.php

--- login.php
+++ login.php
@@ -84,6 +84,12 @@ if (isset($_POST['login'])) {
     }
 
 }
+     $request = $_SERVER['REQUEST_URI'];
+if (substr($request, -4) == '.php') {
+    $new_url = substr($request, 0, -4);
+    header("Location: $new_url", true, 301);
+    exit();
+}
 ?>
 
 <!DOCTYPE html>
