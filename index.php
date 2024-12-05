386a2b074520737103a569b2c981ef20148aa20e
shanine01234 <rafaelpacino43@gmail.com>
2024-12-05 14:46:13 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php 
 require_once('inc/header.php');
-.
- $request = $_SERVER['REQUEST_URI'];
-if (substr($request, -4) == '.php') {
-    $new_url = substr($request, 0, -4);
-    header("Location: $new_url", true, 301);
-    exit();
-}
 ?>
 
 <!DOCTYPE html>
