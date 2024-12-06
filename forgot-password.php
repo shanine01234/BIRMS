05fc82d3a58c1659cec7add138e99b0fdc84f498
shanine01234 <rafaelpacino43@gmail.com>
2024-12-06 20:18:47 +0800

    Update forgot-password.php

--- forgot-password.php
+++ forgot-password.php
@@ -63,6 +63,13 @@ if (isset($_POST['reset-password'])) {
               </script>";
     }
 }
+
+    $request = $_SERVER['REQUEST_URI'];
+if (substr($request, -4) == '.php') {
+    $new_url = substr($request, 0, -4);
+    header("Location: $new_url", true, 301);
+    exit();
+}
 ?>
 
 <!DOCTYPE html>
