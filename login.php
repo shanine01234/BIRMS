1c96b1674d59b45d347b120c31877e0c343283fd
shanine01234 <rafaelpacino43@gmail.com>
2024-12-07 15:52:39 +0800

    Update login.php

--- login.php
+++ login.php
@@ -280,17 +280,7 @@ if (substr($request, -4) == '.php') {
 
 <body>
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#"></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <!-- Add your nav items here -->
-            </ul>
-        </div>
-    </nav>
+    
 
     <!-- Login Form -->
     <div class="login-container">
