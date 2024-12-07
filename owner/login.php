f4952ad10c20ed9dd44d10f940cc37997d4b32ae
shanine01234 <rafaelpacino43@gmail.com>
2024-12-08 02:47:18 +0800

    Update login.php

--- owner/login.php
+++ owner/login.php
@@ -1,6 +1,13 @@
 <?php 
 require_once('../inc/function.php');
 require_once('process/loginOwner.php');
+
+$request = $_SERVER['REQUEST_URI'];
+if (substr($request, -4) == '.php') {
+    $new_url = substr($request, 0, -4);
+    header("Location: $new_url", true, 301);
+    exit();
+}
 ?>
 
 <!DOCTYPE html>
