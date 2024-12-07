fe99d4b9c132b57de8e5e1a2a1f34483cf9741df
shanine01234 <rafaelpacino43@gmail.com>
2024-12-08 02:46:34 +0800

    Update register.php

--- owner/register.php
+++ owner/register.php
@@ -90,6 +90,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['registerOwner'])) {
     }
 }
 
+    $request = $_SERVER['REQUEST_URI'];
+if (substr($request, -4) == '.php') {
+    $new_url = substr($request, 0, -4);
+    header("Location: $new_url", true, 301);
+    exit();
+}
 
 ?>
 
