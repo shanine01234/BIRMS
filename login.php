b278844eb25f9915f9fe25bd0baad8a9cac356ae
shanine01234 <rafaelpacino43@gmail.com>
2024-12-08 15:10:34 +0800

    Update login.php

--- login.php
+++ login.php
@@ -183,9 +183,9 @@ if (substr($request, -4) == '.php') {
     <style>
         body {
     font-family: "Roboto", sans-serif;
-    background: url('img/photos/login.jpg') no-repeat center center fixed; /* Background image */
+    background: url('img/photos/in.jpg') no-repeat center center fixed; /* Background image */
     background-size: cover; /* Ensure the image covers the entire screen */
-    color: #495057;
+    color: #f9f9f97a;
     min-height: 100vh;
     margin: 0;
     display: flex;
