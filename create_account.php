597e5d380bf56742b10e8a9ac111b18401b9d41a
shanine01234 <rafaelpacino43@gmail.com>
2024-12-10 02:51:00 +0800

    commt

--- create_account.php
+++ create_account.php
@@ -58,7 +58,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $verification_code = random_int(10000, 99999);
 
     // Prepare and bind
-    $stmt = $conn->prepare("INSERT INTO users (username, email, password, contact, verification_code, status) VALUES (?, ?, ?, ?, ?, ?)");
+    $stmt = $conn->prepare("INSERT INTO users (username, email, password, contact, code, status) VALUES (?, ?, ?, ?, ?, ?)");
     $status = 0; // 0 for unverified
     $stmt->bind_param("sssssi", $name, $email, $hashed_password, $contact, $verification_code, $status);
 
