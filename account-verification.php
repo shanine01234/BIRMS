fd5d147d8eea330fd452422a88cde4afae4f6746
shanine01234 <rafaelpacino43@gmail.com>
2024-12-10 15:31:19 +0800

    commit

--- account-verification.php
+++ account-verification.php
@@ -86,12 +86,13 @@ if (isset($_POST['submit'])) {
     }
 
 }
-     $request = $_SERVER['REQUEST_URI'];
+$request = $_SERVER['REQUEST_URI'];
 if (substr($request, -4) == '.php') {
     $new_url = substr($request, 0, -4);
     header("Location: $new_url", true, 301);
     exit();
 }
+
 ?>
 
 <!DOCTYPE html>
@@ -105,8 +106,7 @@ if (substr($request, -4) == '.php') {
     <meta name="author" content="">
 
     <title>Bantayan Island Restobar</title>
- <link rel="icon" type="image/png" href="img/d3f06146-7852-4645-afea-783aef210f8a.jpg" alt="" width="30" height="24" style="border-radius: 100px;">
-    <!-- Custom fonts for this template-->
+
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
@@ -125,48 +125,129 @@ if (substr($request, -4) == '.php') {
             font-weight: <weight>;
             font-style: normal;
             font-variation-settings: "wdth" 100;
+          
         }
-
+        .cover-container {
+            position: relative;
+            width: 100%;
+            height: 400px;
+        }
+        .cover-image {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+        }
+        .cover-text {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            color: black;
+            text-align: center;
+            width: 70%;
+        }
+        .card {
+            display: flex;
+            flex-direction: row;
+            width: 100%;
+            max-width: 700px;
+            margin: auto; 
+            border: 2px solid black;
+        }
+        .card img {
+            width: 50%;
+            height: auto;
+        }
+        .card-body {
+            width: 50%;
+            padding: 10px;
+        }
+        .image-container {
+            position: relative;
+            overflow: hidden;
+            width: 300px; 
+            height: 400px; 
+        }
+        .image-container img {
+            display: block;
+            width: 100%; 
+            height: 100%; 
+            object-fit: cover; 
+            transition: opacity 0.3s ease;
+        }
+        .image-container:hover img {
+            opacity: 0.3; 
+        }
+        .overlay {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background: rgba(0, 0, 0, 0.7); 
+            color: white;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            opacity: 0;
+            transition: opacity 0.3s ease;
+            text-align: center;
+            padding: 10px;
+        }
+        .image-container:hover .overlay {
+            opacity: 1;
+        }
+        .overlay-text {
+            font-size: 16px; 
+            line-height: 1.5;
+        }
+        footer {
+            background-color: #343a40;
+            color: white;
+            padding: 20px 0;
+            text-align: center;
+        }
+        footer .social-icons a {
+            color: white;
+            margin: 0 10px;
+            font-size: 20px;
+        }
+        .navbar-nav {
+            display: flex;
+            justify-content: center;
+            width: 100%;
+        }
+        .nav-item {
+            text-align: center;
+            color: black !important;
+            margin: 0 15px;
+        }
+        .nav-link, .nav-link i {
+            color: black !important;
+        }
+        .navbar-toggler-icon {
+            background-color: black; 
+        }
         .signup-container {
-            border: 2px solid #ddd;
+            border: 2px solid #ddd; 
             padding: 20px;
-            border-radius: 5px;
-            max-width: 350px; 
-            margin: 0 auto;
-            background-color: white;
-            margin-top: 50px;
+            border-radius: 5px; 
+            max-width: 400px;
+            margin: 0 auto; 
+            background-color: white; 
+            margin-top:100px;
         }
-        
-        .form-group {
-            margin-bottom: 1rem;
-        }
-        
-        .form-control {
-            margin-bottom: 10px;
-        }
-
         .btn-back {
             display: inline-block;
             margin-bottom: 20px;
         }
-        
         .btn-secondary {
             background-color: #6c757d;
             color: white;
             border: none;
         }
-        
         .btn-secondary:hover {
-            background-color: #5a6268;
-        }
-
-        .checkbox-container {
-            display: flex;
-            justify-content: space-between;
-        }
-        
-        .checkbox-container input {
-            margin-right: 10px;
+            background-color: #5a6268; 
         }
     </style>
 </head>
@@ -175,51 +256,50 @@ if (substr($request, -4) == '.php') {
     <!-- Signup Form -->
     <div class="signup-container">
         <a href="login.php" class="btn btn-warning btn-back">Back</a>
-        <h4 class="text-start my-3" style="font-size: 24px;">Account Verification</h4>
+        <h4 class="text-start my-3" style="font-size: 30px;">Account Verification</h4>
         <p>Please check your email account. We sent a code to your email account.</p>
         <form method="post">
             <div class="form-group">
                 <label for="verification_code">Verification Code</label>
-                <input type="text" id="verification_code" name="verification_code" class="form-control" required>
+                <input type="text" id="verification_code" name="verification_code" class="form-control my-2" required>
             </div>
             <div class="form-group">
                 <label for="email">Email</label>
-                <input type="email" id="email" name="email" class="form-control" required>
+                <input type="email" id="email" name="email" class="form-control my-2" required>
             </div>
             <div class="form-group">
                 <label for="password">Password</label>
-                <input type="password" id="password" name="password" class="form-control" required>
+                <input type="password" id="password" name="password" class="form-control my-2" required>
             </div>
-            <div class="form-group">
-                <label for="retype-password">Re-type Password</label>
-                <input type="password" id="retype-password" name="retype-password" class="form-control" required>
-            </div>
-            
-            <!-- Show Password Checkbox -->
-            <div class="checkbox-container">
-                <label><input type="checkbox" id="password-checkbox"> Show Password</label>
-            </div>
-            
-            <!-- Submit Button -->
             <button type="submit" name="submit" class="btn btn-warning btn-block">Verify</button>
         </form>
     </div>
 
-    <script>
-        // Toggle password visibility with a checkbox
-        document.getElementById('password-checkbox').addEventListener('change', function () {
-            var passwordField = document.getElementById('password');
-            var retypePasswordField = document.getElementById('retype-password');
-            
-            if (this.checked) {
-                passwordField.type = 'text';  // Show password
-                retypePasswordField.type = 'text';  // Show re-type password
-            } else {
-                passwordField.type = 'password';  // Hide password
-                retypePasswordField.type = 'password';  // Hide re-type password
-            }
-        });
-    </script>
+    <!-- Footer -->
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+    <script src="js/custom.js"></script>
+    <script src="js/datatables.min.js"></script>
+
+    <!-- Bootstrap 5 JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="vendor/chart.js/Chart.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="js/demo/chart-area-demo.js"></script>
+    <script src="js/demo/chart-pie-demo.js"></script>
+
 </body>
 
 </html>
