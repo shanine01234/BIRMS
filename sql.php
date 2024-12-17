bc70cf0f34d916b0b0bd627b314c3eeccf4988ac
shanine01234 <rafaelpacino43@gmail.com>
2024-12-17 22:31:06 +0800

    Update sql.php

--- sql.php
+++ sql.php
@@ -13,8 +13,6 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// SQL query to add the 'proof' column to the 'orders' table
-$sql = "ALTER TABLE orders ADD COLUMN proof VARCHAR(255)";
 
 // Execute the query
 if ($conn->query($sql) === TRUE) {
