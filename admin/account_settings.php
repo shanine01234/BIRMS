f06ff4f51cf63ca105a6712965a1175d952ca8d0
shanine01234 <rafaelpacino43@gmail.com>
2024-12-19 01:21:50 +0800

    commit

--- admin/account_settings.php
+++ admin/account_settings.php
@@ -240,7 +240,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             text-transform: uppercase;
             transition: 0.5s;
             background-size: 200% auto;
-            color: white;
+            color: darkslategrey;
             box-shadow: 0 0 20px #eee;
             border-radius: 10px;
             display: block;
