dab7b34e1f7c6679c0a21b88cb5f01919e6e1306
shanine01234 <rafaelpacino43@gmail.com>
2024-12-18 22:57:57 +0800

    commit

--- owner/menu.php
+++ owner/menu.php
@@ -352,15 +352,7 @@ if (!isset($_SESSION['owner_id'])) {
             </div>
             <!-- End of Main Content -->
 
-            <!-- Footer -->
-            <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>Group &copy; BSIT</span>
-                    </div>
-                </div>
-            </footer>
-            <!-- End of Footer -->
+            
 
         </div>
         <!-- End of Content Wrapper -->
