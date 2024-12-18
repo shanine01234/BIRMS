1b54964203e219cd8b4634a27e1c6f2208d0c509
shanine01234 <rafaelpacino43@gmail.com>
2024-12-18 23:12:03 +0800

    commit

--- owner/menu.php
+++ owner/menu.php
@@ -173,7 +173,7 @@ if (!isset($_SESSION['owner_id'])) {
 
                     <!-- Add Menu Button -->
 <!-- Add Menu Button -->
-<button type="button" class="btn btn-primary mb-3" data-toggle="modal" data-target="#addMenuModal" style="width: 950px;">
+<button type="button" class="btn btn-primary mb-3" data-toggle="modal" data-target="#addMenuModal" style="margin-left: 950px;">
     <i class="fas fa-plus"></i> Add to Menu
 </button>
 
