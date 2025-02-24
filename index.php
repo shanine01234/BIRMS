51418cf8f20d1c2532b7df19128fb9c88ad07f4e
James Vincent Pastorillo <pastorillo.james25@gmail.com>
2025-02-24 15:39:34 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Erwin Myaot</title>
+    <title>Bantayan Restobars</title>
     <style>
         body {
             margin: 0;
